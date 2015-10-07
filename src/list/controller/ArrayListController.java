f304d19b38e9703502823414fb860298d7c417fc
ghop1073 <mhop1073@740s-pl-109.local>
2015-10-07 13:31:12 -0600

    resolved errors and made Viewer

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,10 +1,18 @@
 package list.controller;
 
+import java.util.ArrayList;
+
 public class ArrayListController
 {
-
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("djahfa8usfhaisufhaisufhkjsafhniusahf");
 	}
 }
---
+++ src/list/controller/ListViewer.java
@@ -0,0 +1,12 @@
+package list.controller;
+
+import javax.swing.JOptionPane;
+
+public class ListViewer
+{
+
+	public void showList(String currentWords)
+	{
+		JOptionPane.showMessageDialog(null, currentWords);
+	}
+}
